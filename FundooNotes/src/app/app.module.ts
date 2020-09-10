fdd939d86f1ba1fb8fc0349ca5a35b1378398788
guptanikhil801 <guptanikhil801@gmail.com>
2020-09-10 18:51:59 +0530

    [Nikhil]removed matgrid removed

--- FundooNotes/src/app/app.module.ts
+++ FundooNotes/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule, MatGridListModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -70,7 +70,6 @@ import { NotesComponent } from './components/notes/notes.component';
     MatTooltipModule,
     MatSnackBarModule,
     MatMenuModule,
-    MatGridListModule
   ],
   providers: [HttpService, UserService,NoteService],
   bootstrap: [AppComponent]
--- FundooNotes/src/app/components/display-note/display-note.component.ts
+++ FundooNotes/src/app/components/display-note/display-note.component.ts
@@ -7,16 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DisplayNoteComponent implements OnInit {
   @Input() listOfNotes: Object[];
-  breakpoint: number;
   constructor() { }
-  disp() {
-    console.log(this.listOfNotes);
-  }
+
   ngOnInit() {
-    this.breakpoint = (window.innerWidth <= 600) ? 1 : 3;
-  }
-  onResize(event) {
-    this.breakpoint = (event.target.innerWidth <= 600) ? 1 : 3;
   }
 
 }
