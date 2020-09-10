d9928ca297b3de93abbca31efcb3db3c536c140b
guptanikhil801 <guptanikhil801@gmail.com>
2020-09-10 22:51:54 +0530

    [Nikhil]add  function created for color change as well as color codes added

--- FundooNotes/src/app/components/createnote/createnote.component.ts
+++ FundooNotes/src/app/components/createnote/createnote.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NoteService } from '../../services/note.service';
-import {  FormGroup,  FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-createnote',
@@ -11,6 +11,7 @@ import {  FormGroup,  FormControl } from '@angular/forms';
 export class CreatenoteComponent implements OnInit {
   isShow = false;
   ishidden = false;
+  notecolor = 'white';
   toggleDisplay() {
     this.isShow = !this.isShow;
     this.ishidden = !this.ishidden;
@@ -20,26 +21,32 @@ export class CreatenoteComponent implements OnInit {
     return this.note.get(inputElement).value;
   }
 
+  changecolor(colorname: string) {
+    this.notecolor = colorname;
+  }
+
   note: FormGroup;
   constructor(private service: NoteService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.note = new FormGroup({
       title: new FormControl(),
-      description: new FormControl()
+      description: new FormControl(),
     });
   }
 
-  onSubmit(): void{
+  onSubmit(): void {
     let data = {
       title: this.inputValue('title'),
-      description: this.inputValue('description')
+      description: this.inputValue('description'),
+       color: this.notecolor
     }
     this.service.createnoteapi(data).subscribe();
     console.log(data);
+     this.notecolor = 'white';
     this.toggleDisplay();
   }
-  
+
 
 }
