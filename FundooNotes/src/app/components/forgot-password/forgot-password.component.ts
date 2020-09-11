36832b2d20478a40efad24a6ebad37237b991435
guptanikhil801 <guptanikhil801@gmail.com>
2020-09-11 15:12:44 +0530

    [Nikhil]new api integration done for forgot-password done

--- FundooNotes/src/app/components/forgot-password/forgot-password.component.ts
+++ FundooNotes/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  user: FormGroup;
+  constructor(private userservice: UserService, private snackBar: MatSnackBar,
+              private fb:FormBuilder) { }
 
-  constructor() { }
-
+  submitdata() {
+    let data = {
+      email:this.inputValue('email')
+    }
+    console.log(data);
+    this.userservice.forgotpasswordapi(data).subscribe(
+      response => {
+        this.snackBar.open("Successfull", "a link has been sent to your email", {
+          duration: 2500
+        });
+      },
+      error => {
+        this.snackBar.open("Failed", "fill email correctly", {
+          duration: 2500
+        });
+      }
+    );
+  }
+  inputValue(inputElement: any) {
+    return this.user.get(inputElement).value;
+  }
   ngOnInit() {
+    this.user =this.fb.group({
+      email:['',
+        [Validators.required,Validators.email,Validators.pattern('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\.([a-zA-Z]{2,5})$')]]
+      });
   }
 
 }
